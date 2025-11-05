2ebd3c82bb3f862d1debd64602556bb1247055f1
pedrooaj <pedroantonio5735@gmail.com>
2025-11-05 16:41:47 -0300

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+
+@Module({
+  imports: [JwtModule.register({
+    secret: "MEU_SEGREDO",
+    signOptions: { expiresIn: "1d" }
+  })],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService]
+})
+export class AuthModule {}
